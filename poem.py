831a766abf98780c6c6c2e637a311736c714c2f5
robinpdvoets <70946671+robinpdvoets@users.noreply.github.com>
2020-10-02 14:08:48 +0200

    Eerste Poem

---
+++ poem.py
@@ -0,0 +1,10 @@
+vers1 = "Mijn voornaam is Robin en mij achternaam is Voets."
+vers2 = "Met prinsjesdag rijden de koning en de koningin in een koets."
+vers3 = "Ik ga graag naar de maccie ookal verdien ik geen enkel stackie."
+vers4 = "In Minecraft download ik een packie"
+vers5 = "Van een pizza vraag ik vaak een piecie"
+vers6 = "Als schoenen koop ik Nike, Off-White en Adidas Yeezy"
+vers7 = "Ik heb een Samsung Galaxy S20"
+vers8 = "Mijn vrienden die zijn veeltintig."
+
+print(vers1,vers2,vers3,vers4,vers5,vers6,vers7,vers8)
